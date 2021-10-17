c87f1a485f5ffe3c774335687674dc68d9f40e95
Nicky Lim <nickylim.p@gmail.com>
2021-10-17 13:14:27 +0800

    added tests choices

--- app/tests/test_choices.py
+++ app/tests/test_choices.py
@@ -8,6 +8,36 @@ def setup():
         "type": "radio",
         "description": "question1"
     })
+    client.post("/questions/", json={
+        "type": "textbox",
+        "description": "question2"
+    })
+
+
+def test_get():
+    client.post("/choices/", json={
+        "description": "choice1",
+        "question_id": 1
+    })
+
+    client.post("/choices/", json={
+        "description": "choice2",
+        "question_id": 1
+    })
+
+    response = client.get("/choices/")
+    assert response.status_code == 200
+    assert response.json() == [{
+        "id": 1, "description": "choice1", "question_id": 1
+    }, {
+        "id": 2, "description": "choice2", "question_id": 1
+    }]
+
+    response = client.get("/choices/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1, "description": "choice1", "question_id": 1
+    }
 
 
 def test_post():
@@ -24,6 +54,27 @@ def test_post():
     }
 
 
+def test_delete():
+    client.post("/choices/", json={
+        "description": "choice1",
+        "question_id": 1
+    })
+
+    response = client.delete("/choices/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "description": "choice1",
+        "question_id": 1
+    }
+
+    response = client.get("/choices/1")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Choice with id of 1 not found"
+    }
+
+
 def test_get_invalid_id():
     response = client.get("/choices/1")
     assert response.status_code == 404
@@ -32,14 +83,33 @@ def test_get_invalid_id():
     }
 
 
-def test_post_invalid_question_id():
+def test_post_invalid_question():
+    # Add to question that does not exist
+    response = client.post("/choices/", json={
+        "description": "choice1",
+        "question_id": 99
+    })
+    assert response.status_code == 400
+    assert response.json() == {
+        "detail": "Question with id of 99 not found"
+    }
+
+    # Add a choice to textbox
     response = client.post("/choices/", json={
         "description": "choice1",
         "question_id": 2
     })
     assert response.status_code == 400
     assert response.json() == {
-        "detail": "Question with id of 2 not found"
+        "detail": "The type of question with id of 2 is a text"
+    }
+
+
+def test_delete_invalid():
+    response = client.delete("/choices/1")
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "Choice with id of 1 not found"
     }
 
 
